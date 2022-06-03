174f80101e36da6186233088486f582971ea687c
aeropagz <klaas.pelzer@gmx.de>
2022-06-03 16:40:17 +0200

    added prints

--- src/main/java/com/example/aufgabe_8/crypto/AesCrypto.java
+++ src/main/java/com/example/aufgabe_8/crypto/AesCrypto.java
@@ -6,6 +6,7 @@ import javax.crypto.*;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
@@ -42,10 +43,15 @@ public class AesCrypto {
         byte[] salt = new byte[PBKDF2_SALT_SIZE];
         rand.nextBytes(salt);
 
+        System.out.println("Salt: " + DatatypeConverter.printHexBinary(salt).toUpperCase());
+
         // Create an instance of PBKDF2 and derive a key.
         PBEKeySpec pwSpec = new PBEKeySpec(password.toCharArray(), salt, PBKDF2_ITERATIONS, ALGORITHM_KEY_SIZE);
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(PBKDF2_NAME);
         byte[] key = keyFactory.generateSecret(pwSpec).getEncoded();
+
+        System.out.println("Generated Key: " + DatatypeConverter.printHexBinary(key).toUpperCase());
+        System.out.println("Encrypting...");
 
         // Encrypt and prepend salt.
         byte[] ciphertextAndNonce = encrypt(plaintext.getBytes(StandardCharsets.UTF_8), key);
@@ -70,10 +76,13 @@ public class AesCrypto {
         System.arraycopy(ciphertextAndNonceAndSalt, 0, salt, 0, salt.length);
         System.arraycopy(ciphertextAndNonceAndSalt, salt.length, ciphertextAndNonce, 0, ciphertextAndNonce.length);
 
+        System.out.println("Extracted salt: " + DatatypeConverter.printHexBinary(salt).toUpperCase());
         // Create an instance of PBKDF2 and derive the key.
         PBEKeySpec pwSpec = new PBEKeySpec(password.toCharArray(), salt, PBKDF2_ITERATIONS, ALGORITHM_KEY_SIZE);
         SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(PBKDF2_NAME);
         byte[] key = keyFactory.generateSecret(pwSpec).getEncoded();
+
+        System.out.println("Generated Key: " + DatatypeConverter.printHexBinary(key).toUpperCase());
 
         // Decrypt and return result.
         return new String(decrypt(ciphertextAndNonce, key), StandardCharsets.UTF_8);
@@ -87,6 +96,8 @@ public class AesCrypto {
         byte[] nonce = new byte[ALGORITHM_NONCE_SIZE];
         rand.nextBytes(nonce);
 
+        System.out.println("Nonce: " + DatatypeConverter.printHexBinary(nonce).toUpperCase());
+
         // Create the cipher instance and initialize.
         Cipher cipher = Cipher.getInstance(ALGORITHM_NAME);
         cipher.init(Cipher.ENCRYPT_MODE,
@@ -96,6 +107,8 @@ public class AesCrypto {
 
         // Encrypt and prepend nonce.
         byte[] ciphertext = cipher.doFinal(plaintext);
+
+        System.out.println("Cipher: " + DatatypeConverter.printHexBinary(ciphertext).toUpperCase());
         byte[] ciphertextAndNonce = new byte[nonce.length + ciphertext.length];
         System.arraycopy(nonce, 0, ciphertextAndNonce, 0, nonce.length);
         System.arraycopy(ciphertext, 0, ciphertextAndNonce, nonce.length, ciphertext.length);
@@ -107,10 +120,15 @@ public class AesCrypto {
             throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException,
             BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
         // Retrieve the nonce and ciphertext.
+        System.out.println("Decrypting...");
         byte[] nonce = new byte[ALGORITHM_NONCE_SIZE];
         byte[] ciphertext = new byte[ciphertextAndNonce.length - ALGORITHM_NONCE_SIZE];
         System.arraycopy(ciphertextAndNonce, 0, nonce, 0, nonce.length);
         System.arraycopy(ciphertextAndNonce, nonce.length, ciphertext, 0, ciphertext.length);
+
+        System.out.println("Extracted nonce: " + DatatypeConverter.printHexBinary(nonce).toUpperCase());
+        System.out.println("Extracted data: " + DatatypeConverter.printHexBinary(ciphertext).toUpperCase());
+
 
         // Create the cipher instance and initialize.
         Cipher cipher = Cipher.getInstance(ALGORITHM_NAME);
@@ -123,6 +141,7 @@ public class AesCrypto {
     }
 
     public void encryptFile() {
+        System.out.println("Verschlüssle Datei");
         try {
             secret.read();
             key.read();
@@ -131,6 +150,7 @@ public class AesCrypto {
             String pass = new String(key.getData(), StandardCharsets.UTF_8);
 
             String encryptString = encryptString(data, pass);
+            System.out.println("Write to file");
             out.write(encryptString);
 
         } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException |
@@ -150,6 +170,7 @@ public class AesCrypto {
             String pass = new String(key.getData(), StandardCharsets.UTF_8);
 
             String decryptString = decryptString(data, pass);
+            System.out.println("Write to file");
             out.write(decryptString);
 
         } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException |
